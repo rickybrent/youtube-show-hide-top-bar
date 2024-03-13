2aedc3b7b0e71716c495a57b591dd0e4a5b46951
azizLIGHT <azizLIGHT@users.noreply.github.com>
2024-03-13 07:02:48 -0400

    Update version

--- YouTube-show-hide-top-header-search-bar.user.js
+++ YouTube-show-hide-top-header-search-bar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube show/hide top header search bar
 // @namespace    https://github.com/azizLIGHT/youtube-show-hide-top-bar
-// @version      1.0
+// @version      1.0.1
 // @description  Hides the top masthead initially and shows it when mousing over the top of the page (excluding fullscreen mode).
 // @match        https://www.youtube.com/watch?v=*
 // @grant        GM_addStyle
